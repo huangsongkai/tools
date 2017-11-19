24082e9132df414b535e6d22a1d6a76dcc61621e
huangsongkai <296731066@qq.com>
2017-11-19 16:35:47 +0800

    Initial commit

---
+++ security/src/main/java/com/cputech/modules/usermsg/config/UsrMsgPagesConfig.java
@@ -0,0 +1,24 @@
+package com.cputech.modules.usermsg.config;
+
+/**
+ * Created by song on 2017/9/25.
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class UsrMsgPagesConfig extends WebMvcConfigurerAdapter {
+    static {
+        System.out.println("WebMvcConfigurerAdapter");
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/logout").setViewName("usr/hello");
+        registry.addViewController("/admin").setViewName("usr/hello");
+        registry.addViewController("/login").setViewName("user/login");
+    }
+}
